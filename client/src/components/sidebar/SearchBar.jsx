313aa5df68364ef22a75cff50d53751b3cd0cef1
Palak Agrawal <palakagrawal899@gmail.com>
2026-07-08 18:17:48 +0530

    Rename Searchbar to SearchBar

--- client/src/components/sidebar/SearchBar.jsx
+++ client/src/components/sidebar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import SearchBar from "./Searchbar";
-const Searchbar = ({
+
+const SearchBar = ({
   users,
   setFilteredUsers,
 }) => {
@@ -23,9 +23,7 @@ const Searchbar = ({
 
   return (
     <div className="p-4">
-
       <div className="flex items-center gap-3 bg-slate-800 border border-slate-700 rounded-xl px-4 py-3 focus-within:border-cyan-500 transition">
-
         <FiSearch
           size={20}
           className="text-slate-400"
@@ -38,9 +36,7 @@ const Searchbar = ({
           onChange={handleSearch}
           className="bg-transparent flex-1 outline-none text-white placeholder:text-slate-500"
         />
-
       </div>
-
     </div>
   );
 };
