af6535cd4ff36d7007c8f48f8ee7ba0a6db86de2
Palak Agrawal <palakagrawal899@gmail.com>
2026-07-08 17:32:08 +0530

    Production URLs

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://pulse-chat-backend-deh0.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  console.log("======================");
-  console.log("🚀 Axios Request");
-  console.log("URL:", config.baseURL + config.url);
-  console.log("Method:", config.method);
-  console.log("Body:", config.data);
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
-  console.log("Headers:", config.headers);
 
   return config;
 });
--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,42 +2,23 @@ import { io } from "socket.io-client";
 
 let socket = null;
 
-// ==========================
-// Connect Socket
-// ==========================
-
 export const connectSocket = (userId) => {
-  if (socket?.connected) {
-    return socket;
-  }
+  if (socket?.connected) return socket;
 
-  socket = io("http://localhost:5000", {
-    query: {
-      userId,
-    },
-    transports: ["websocket"],
-  });
-
-  socket.on("connect", () => {
-    console.log("🟢 Socket Connected:", socket.id);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("🔴 Socket Disconnected");
-  });
+  socket = io(
+    "https://pulse-chat-backend-deh0.onrender.com",
+    {
+      query: {
+        userId,
+      },
+      transports: ["websocket"],
+    }
+  );
 
   return socket;
 };
 
-// ==========================
-// Get Socket
-// ==========================
-
 export const getSocket = () => socket;
-
-// ==========================
-// Disconnect Socket
-// ==========================
 
 export const disconnectSocket = () => {
   if (socket) {
